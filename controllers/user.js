f2fe1b4f0d905ee79f93e917ef44ba6a1a32c9cc
gentlesoul18 <oyebamijibarakat7@gmail.com>
2023-11-18 14:54:18 +0100

    fix: added try/catch block to user controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,58 @@
+const boom = require('boom');
+
 const User = require('../models/user')
 
 
 exports.getUsers = async (request, reply) => {
-    const users = await User.find({deleted: false})
-    return users
+    try {
+        const users = await User.find({ deleted: false });
+        return users;
+    } catch (error) {
+        throw boom.boomify(error);
+    }
 }
 
 exports.getUser = async (request, reply) => {
-    const id = request.params.id
-    const user = await User.findById(id)
-    return user
+    try {
+        const id = request.params.id;
+        const user = await User.findById(id);
+        return user;
+    } catch (error) {
+        throw boom.boomify(error);
+    }
+    
 }
 
 exports.addUser = async (request, reply) => {
-    const user = new User(request.body)
-    user.save()
-    console.log(user)
-    return user
+    try {
+        const user = new User(request.body)
+        user.save()
+        console.log(user)
+        return user
+    } catch (error) {
+        throw boom.boomify(error)
+    }
 }
 
 exports.removeUser = async (request, reply) => {
-    const id = request.params.id
-    const user = await User.findById(id)
-    user.deleted = true
-    user.save()
-    return {message: 'User deleted'}
+    try {
+        const id = request.params.id;
+        const user = await User.findById(id);
+        user.deleted = true;
+        user.save();
+        return { message: "User deleted" };
+    } catch (error) {
+        throw boom.boomify(error);
+    }
 }
 
 exports.updateUser = async (request, reply) => {
-    const id = request.params.id
-    const { ...updateData } = request.body
-    const updateUser = await User.findByIdAndUpdate(id, updateData)
-    return updateUser
+    try {
+        const id = request.params.id
+        const { ...updateData } = request.body
+        const updateUser = await User.findByIdAndUpdate(id, updateData)
+        return updateUser
+    } catch (error) {
+        throw boom.boomify(error);
+    }
 }
